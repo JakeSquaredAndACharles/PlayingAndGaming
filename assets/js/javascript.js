c98159a44c8ebeeb6abd0eb410987847d658ba3a
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-17 16:37:36 -0600

    debugging

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -10,22 +10,23 @@ var showPlaylist = function (id) {
 
     if (buttonSelected === "chilltendo") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4ug4ktSFhisTatqDShUdAR" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
+        console.log("chill brah");
     }
     else if (buttonSelected === "synthwave") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4TeZZfbu3rbifAjwiAAEq8" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
-      
+        console.log("synthy");
     }
     else if (buttonSelected === "superLofi") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/3PeWDrXlXk9utHaK2CpEgT" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
-   
+        console.log("lobrah");
     }
     else if (buttonSelected === "pokeChill") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4lBMa9JEuCSIs3NkPEIwvN" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
-     
+        console.log("poke man");
     }
     else if (buttonSelected === "undertale") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4eSyiMMYG6MRQ6pWkuuDgZ" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
- 
+        console.log("whats under this tale?");
     }
 
 };
