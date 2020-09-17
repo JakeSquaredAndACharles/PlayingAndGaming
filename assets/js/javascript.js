0c56b7caaebf1dbfd5b78e6db26c9ee670558465
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-17 16:30:19 -0600

    added DOM elements

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,24 +1,29 @@
 var playlistEl = document.querySelector('#playlist-container');
+var chilltendoEl = document.querySelector('#chilltendo');
+var synthwaveEl = document.querySelector('#synthwave');
+var superLofiEl = document.querySelector('#superLofi');
+var pokeChillEl = document.querySelector('#pokeChill');
+var undertaleEl = document.querySelector('#undertale');
 
-var showPlaylist = function (data) {
+var showPlaylist = function (id) {
+    var buttonSelected = id;
 
-
-    if (id === "chilltendo") {
+    if (buttonSelected === "chilltendo") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4ug4ktSFhisTatqDShUdAR" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
     }
-    else if (id === "synthwave") {
+    else if (buttonSelected === "synthwave") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4TeZZfbu3rbifAjwiAAEq8" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
       
     }
-    else if (id === "superLofi") {
+    else if (buttonSelected === "superLofi") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/3PeWDrXlXk9utHaK2CpEgT" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
    
     }
-    else if (id === "pokeChill") {
+    else if (buttonSelected === "pokeChill") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4lBMa9JEuCSIs3NkPEIwvN" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
      
     }
-    else if (id === "undertale") {
+    else if (buttonSelected === "undertale") {
         playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4eSyiMMYG6MRQ6pWkuuDgZ" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
  
     }
