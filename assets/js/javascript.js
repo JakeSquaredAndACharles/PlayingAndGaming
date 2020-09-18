e9be9638023cfc60da3a486d3cfd67664e5ee3f1
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-17 19:28:32 -0600

    cleaned up code

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -5,25 +5,25 @@ var superLofiEl = document.querySelector('#superLofi');
 var pokeChillEl = document.querySelector('#pokeChill');
 var undertaleEl = document.querySelector('#undertale');
 
-var showPlaylist1 = function () {
-        playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4ug4ktSFhisTatqDShUdAR" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
-        console.log("chill brah");
+var showPlaylist1 = function() {
+    playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4ug4ktSFhisTatqDShUdAR" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
+    console.log("chill brah");
 };
 
-var showPlaylist2 = function () {
+var showPlaylist2 = function() {
     playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4TeZZfbu3rbifAjwiAAEq8" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
     console.log("synthy");
 };
 
-var showPlaylist3 = function () {
+var showPlaylist3 = function() {
     playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/3PeWDrXlXk9utHaK2CpEgT" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
     console.log("lobrah");
 };
-var showPlaylist4 = function () {
+var showPlaylist4 = function() {
     playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4lBMa9JEuCSIs3NkPEIwvN" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
     console.log("poke man");
 };
-var showPlaylist5 = function () {
+var showPlaylist5 = function() {
     playlistEl.innerHTML = '<iframe class="spotify" src="https://open.spotify.com/album/4eSyiMMYG6MRQ6pWkuuDgZ" frameborder="0" id="music-player" allowtransparency="true" allow="encrypted-media"></iframe>';
         console.log("whats under this tale?");
 };
